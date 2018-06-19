d7d0fefce63f7906f37e256211b6e388492ff6ea
TonyCode2012 <TonyCode2012@126.com>
2018-06-19 10:00:52 +0800

    fix package script

--- mac/wifiApp/scripts/package.sh
+++ mac/wifiApp/scripts/package.sh
@@ -4,14 +4,17 @@ basedir=`dirname $0`
 basedir=`cd $basedir;pwd`
 appDir=$basedir/..
 packageDir=$basedir/wifiApp
-packageName=$appDir/wifiApp-`cat $appDir/VERSION`.zip
+packageName=wifiApp-`cat $appDir/VERSION`.zip
 
 mkdir -p $packageDir
 cp -r $appDir/config $appDir/img $appDir/out $appDir/wpa_setup $packageDir
 
-zip -r $packageName $packageDir
+cd $basedir
+zip -r $packageName wifiApp
 if [ $? -ne 0 ]; then
     echo "[ERROR] package wifiApp failed!" >&2
 fi
+
+mv $packageName ../
 
 rm -rf $packageDir
