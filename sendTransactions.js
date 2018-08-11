c4f19cde88da4a16bb6977070dba7ec43946beea
mryyd <yyd106@126.com>
2018-08-11 18:11:57 +0800

    support double token

--- sendTransactions.js
+++ sendTransactions.js
@@ -28,8 +28,8 @@ exports.sendRawTransfer = function(account, pk, transferTo, amount){
 * others: parameters of the contract
 */
 exports.sendRawDeduction = function(account, pk, apAddr){
-	var aBI = config.ControllerABI_
-	var addr = config.ControllerAddr_
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
 	var httpProvider = config.HttpProvider
 	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
 
@@ -48,8 +48,8 @@ exports.sendRawDeduction = function(account, pk, apAddr){
 * others: parameters of the contract
 */
 exports.sendRawCommission = function(account, pk, apAddr, advAddr){
-	var aBI = config.ControllerABI_
-	var addr = config.ControllerAddr_
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
 	var httpProvider = config.HttpProvider
 	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
 
@@ -68,8 +68,8 @@ exports.sendRawCommission = function(account, pk, apAddr, advAddr){
 * others: parameters of the contract
 */
 exports.sendRawWatchAdv = function(account, pk, advAddr, apAddr){
-	var aBI = config.ControllerABI_
-	var addr = config.ControllerAddr_
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
 	var httpProvider = config.HttpProvider
 	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
 
@@ -89,8 +89,8 @@ exports.sendRawWatchAdv = function(account, pk, advAddr, apAddr){
 * others: parameters of the contract
 */
 exports.sendRawAdvSetPrice = function(account, pk, advAddr, price){
-	var aBI = config.ControllerABI_
-	var addr = config.ControllerAddr_
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
 	var httpProvider = config.HttpProvider
 	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
 
@@ -109,8 +109,8 @@ exports.sendRawAdvSetPrice = function(account, pk, advAddr, price){
 * others: parameters of the contract
 */
 exports.sendRawApSetPrice = function(account, pk, apaddr, price){
-	var aBI = config.ControllerABI_
-	var addr = config.ControllerAddr_
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
 	var httpProvider = config.HttpProvider
 	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
 
@@ -120,5 +120,238 @@ exports.sendRawApSetPrice = function(account, pk, apaddr, price){
   	var abi = JSON.parse(aBI);
   	var contractInstant = new web3.eth.Contract(abi, addr);
 	var encodedData = contractInstant.methods.aPSetPrice(apaddr, price).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+//-----------------------   V2, coin related    -------------------------------
+
+//-----------------------   V2, coin related    -------------------------------
+
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawApSetCoin = function(account, pk, apaddr, coin){
+	var aBI = config.CoinControllerABI
+	var addr = config.CoinControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.aPSetCoin(apaddr, coin).encodeABI()
+	console.log(encodedData)
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawApRegister = function(account, pk, apaddr, bssid){
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.aPRegister(apaddr, bssid).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawThingRegister = function(account, pk, thingaddr, price){
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.thingRegister(thingaddr, price).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawAdvSetCoin = function(account, pk, advaddr, coin){
+	var aBI = config.CoinControllerABI
+	var addr = config.CoinControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.advSetCoin(advaddr, coin).encodeABI()
+	console.log(encodedData)
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawThingSetCoin = function(account, pk, thingaddr, coin){
+	var aBI = config.CoinControllerABI
+	var addr = config.CoinControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.thingSetCoin(thingaddr, coin).encodeABI()
+	console.log(encodedData)
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawAdvRegister = function(account, pk, advaddr){
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.advRegister(advaddr).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawCoinTransfer = function(account, pk, transferTo, amount){
+	var aBI = config.CoinABI
+	var addr = config.CoinAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.transfer(transferTo, amount).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawCommissionCoin = function(account, pk, apAddr, advAddr){
+	var aBI = config.CoinControllerABI
+	var addr = config.CoinControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.commissionCoin(apAddr, advAddr).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawWatchAdvCoin = function(account, pk, advAddr, apAddr){
+	var aBI = config.CoinControllerABI
+	var addr = config.CoinControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.watchAdvCoin(advAddr, apAddr).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawBuyThingCoin = function(account, pk, thingaddr){
+	var aBI = config.CoinControllerABI
+	var addr = config.CoinControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.buyThingCoin(thingaddr).encodeABI()
+    sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
+}
+
+/*
+* account: caller's address
+* pk: caller's private key string
+* others: parameters of the contract
+*/
+exports.sendRawBuyThing = function(account, pk, thingaddr){
+	var aBI = config.ControllerABI
+	var addr = config.ControllerAddr
+	var httpProvider = config.HttpProvider
+	var web3 = new Web3(new Web3.providers.HttpProvider(httpProvider));
+
+	var privateKey = new Buffer(pk, 'hex')
+	console.log("coinbase:")
+	web3.eth.getCoinbase((err, coinbase) => {console.log(coinbase)})
+  	var abi = JSON.parse(aBI);
+  	var contractInstant = new web3.eth.Contract(abi, addr);
+	var encodedData = contractInstant.methods.buyThing(thingaddr).encodeABI()
     sendRawTransaction.sendTransction(web3, privateKey, account, addr, encodedData)
 }
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 config = require("./config")
 transaction = require("./sendTransactions")
 
-
+/*
 // Test transfer
 transaction.sendRawTransfer(
     config.TestAdvOwner, 
@@ -9,7 +9,7 @@ transaction.sendRawTransfer(
     config.TestUser1, 
     100
 )
-
+*/
 
 /*
 transaction.sendRawCommission(
@@ -44,8 +44,8 @@ transaction.sendRawAdvSetPrice(
     config.TestAdvOwnerPK, 
     config.TestAdvAddr,
     30
-)*/
-
+)
+*/
 
 /*
 transaction.sendRawApSetPrice(
@@ -55,3 +55,94 @@ transaction.sendRawApSetPrice(
     10
 )
 */
+
+
+/*
+transaction.sendRawApRegister(
+    config.TestApOwner, 
+    config.TestApOwnerPK, 
+    config.TestApAddr2,
+    "testBSSID2"
+)
+*/
+
+/*
+transaction.sendRawApSetCoin(
+    config.TestApOwner, 
+    config.TestApOwnerPK, 
+    config.TestApAddr2,
+    10
+)
+*/
+
+
+
+/*
+transaction.sendRawAdvRegister(
+    config.TestAdvOwner, 
+    config.TestAdvOwnerPK, 
+    config.TestAdvAddr2
+)
+*/
+
+/*
+transaction.sendRawAdvSetCoin(
+    config.TestAdvOwner, 
+    config.TestAdvOwnerPK, 
+    config.TestAdvAddr2,
+    10
+)
+*/
+
+
+/*
+transaction.sendRawCommissionCoin(
+    config.TestUser1, 
+    config.TestUser1PK, 
+    config.TestApAddr, 
+    config.TestAdvAddr2
+)
+*/
+
+/*
+transaction.sendRawWatchAdvCoin(
+    config.TestUser1, 
+    config.TestUser1PK, 
+    config.TestAdvAddr2,
+    config.TestApAddr
+)
+*/
+
+/*
+transaction.sendRawThingRegister(
+    config.TestAdvOwner, 
+    config.TestAdvOwnerPK, 
+    config.TestThingAddr,
+    5
+)
+*/
+
+/*
+transaction.sendRawThingSetCoin(
+    config.TestAdvOwner, 
+    config.TestAdvOwnerPK, 
+    config.TestThingAddr,
+    10
+)
+*/
+
+/*
+transaction.sendRawBuyThingCoin(
+    config.TestUser1, 
+    config.TestUser1PK, 
+    config.TestThingAddr
+)
+*/
+
+
+transaction.sendRawBuyThing(
+    config.TestUser1, 
+    config.TestUser1PK, 
+    config.TestThingAddr
+)
+
